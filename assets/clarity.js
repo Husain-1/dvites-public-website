1ddcb6c879a991e19e13e2a114f9a685253734fe
Husain Punjabi <husainpunjabi53@gmail.com>
2026-07-17 23:44:02 +0530

    Intalling clarity

---
+++ assets/clarity.js
@@ -0,0 +1,18 @@
+(function (global) {
+  "use strict";
+
+  var CLARITY_ID = "xnzg1ok7zh";
+
+  (function (c, l, a, r, i, t, y) {
+    c[a] =
+      c[a] ||
+      function () {
+        (c[a].q = c[a].q || []).push(arguments);
+      };
+    t = l.createElement(r);
+    t.async = 1;
+    t.src = "https://www.clarity.ms/tag/" + i;
+    y = l.getElementsByTagName(r)[0];
+    y.parentNode.insertBefore(t, y);
+  })(global, document, "clarity", "script", CLARITY_ID);
+})(window);
